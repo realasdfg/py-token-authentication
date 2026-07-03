dec31216dad8639c147ee509292c400fb7a327c2
Roman <realasdfga@gmail.com>
2026-07-04 01:01:14 +0200

    Fix

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -4,10 +4,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (request.user and request.user.is_staff)
-            or (
+            (
                 request.method in SAFE_METHODS
                 and request.user
                 and request.user.is_authenticated
             )
+            or (request.user and request.user.is_staff)
         )
